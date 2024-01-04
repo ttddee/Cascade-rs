cff4fab4c07f4ae1aa3bc8a52d62bed3535e2b6e
ttddee <till.dechent@gmail.com>
2024-01-04 16:53:22 +0000

    Add ScrollArea to properties panel.

--- csc_ui/src/properties_panel.rs
+++ csc_ui/src/properties_panel.rs
@@ -28,35 +28,38 @@ pub fn build_properties_panel(
                     ui.label(egui::RichText::new(node_type.name()).strong());
                     ui.separator();
 
-                    egui::Grid::new("properties_grid")
-                        .num_columns(2)
-                        .spacing([40.0, 4.0])
-                        .striped(true)
-                        .show(ui, |ui| {
-                            for property in &mut active_node.user_data.node_properties {
-                                match property {
-                                    NodeProperty::Float(data) => {
-                                        let range = RangeInclusive::new(*data.min(), *data.max());
-                                        ui.add(egui::Label::new(data.name().clone()));
-                                        ui.add(egui::Slider::new(data.value(), range));
-                                        ui.end_row();
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        egui::Grid::new("properties_grid")
+                            .num_columns(2)
+                            .spacing([40.0, 4.0])
+                            .striped(true)
+                            .show(ui, |ui| {
+                                for property in &mut active_node.user_data.node_properties {
+                                    match property {
+                                        NodeProperty::Float(data) => {
+                                            let range =
+                                                RangeInclusive::new(*data.min(), *data.max());
+                                            ui.add(egui::Label::new(data.name().clone()));
+                                            ui.add(egui::Slider::new(data.value(), range));
+                                            ui.end_row();
+                                        }
+                                        NodeProperty::Choice(data) => {
+                                            let length = data.choices().len();
+                                            let choices = data.choices().clone();
+                                            ui.add(egui::Label::new(data.name()));
+                                            egui::ComboBox::from_id_source("combobox").show_index(
+                                                ui,
+                                                data.index(),
+                                                length,
+                                                |i| &choices[i],
+                                            );
+                                            ui.end_row();
+                                        }
+                                        _ => {}
                                     }
-                                    NodeProperty::Choice(data) => {
-                                        let length = data.choices().len();
-                                        let choices = data.choices().clone();
-                                        ui.add(egui::Label::new(data.name()));
-                                        egui::ComboBox::from_id_source("combobox").show_index(
-                                            ui,
-                                            data.index(),
-                                            length,
-                                            |i| &choices[i],
-                                        );
-                                        ui.end_row();
-                                    }
-                                    _ => {}
                                 }
-                            }
-                        });
+                            });
+                    });
                 }
             }
         });
